5ec6dee254c59f670d4cfdc47101011d30ab7518
Rraaalf <kymyass@gmail.com>
2024-06-27 22:12:31 +0300

    исправление варнингов

--- src/test/java/page/VerificationPage.java
+++ src/test/java/page/VerificationPage.java
@@ -1,14 +1,13 @@
 package page;
 
 import com.codeborne.selenide.SelenideElement;
-import data.DataHelper;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class VerificationPage {
-    private SelenideElement codeField = $("[data-test-id=code] input");
-    private SelenideElement verifyButton = $("[data-test-id=action-verify]");
+    private final SelenideElement codeField = $("[data-test-id=code] input");
+    private final SelenideElement verifyButton = $("[data-test-id=action-verify]");
 
     public VerificationPage() {
         codeField.shouldBe(visible);
