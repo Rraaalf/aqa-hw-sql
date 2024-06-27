60b2584981be9a79779191ed0e2e17cc400a9060
Rraaalf <kymyass@gmail.com>
2024-06-27 22:11:02 +0300

    переименован тестовый класс; добавлен @AfterAll метод cleanUp(); внесены правки в метод loginWithValidData()

--- src/test/java/TestDbInteraction.java
+++ src/test/java/TestDbInteraction.java
@@ -1,12 +1,12 @@
 import com.codeborne.selenide.Configuration;
+import data.DataHelper;
 import mode.DbUtils;
-import mode.User;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.chrome.ChromeOptions;
 import page.DashboardPage;
 import page.LoginPage;
-import page.VerificationPage;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,8 +14,7 @@ import java.util.Map;
 import static com.codeborne.selenide.Selenide.open;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class DbInteraction {
-    User user = new User("vasya", "qwerty123");
+public class TestDbInteraction {
 
     @BeforeEach
     void setup() {
@@ -27,11 +26,16 @@ public class DbInteraction {
         Configuration.browserCapabilities = options;
     }
 
+    @AfterAll
+    static void cleanUp() {
+        DbUtils.wipeEverything();
+    }
+
     @Test
     void loginWithValidData() {
-        open("http://localhost:9999");
-        LoginPage loginPage = new LoginPage();
-        VerificationPage verificationPage = loginPage.validLogin(user);
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
         DashboardPage dashboardPage = verificationPage.validVerify(DbUtils.getVerificationCode());
         assertEquals("Личный кабинет", dashboardPage.getHeading());
     }
