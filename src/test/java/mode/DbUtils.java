0c664a42afcf008dc9b5995a8d8ca4030abf454f
Rraaalf <kymyass@gmail.com>
2024-06-27 22:08:35 +0300

    переработан метод getVerificationCode(); добавлены методы connection() и wipeEverything()

--- src/test/java/mode/DbUtils.java
+++ src/test/java/mode/DbUtils.java
@@ -1,33 +1,35 @@
 package mode;
 
-import lombok.val;
+import lombok.SneakyThrows;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DbUtils {
+    private final static QueryRunner run = new QueryRunner();
+
     public DbUtils() {
     }
 
+    @SneakyThrows
     public static String getVerificationCode() {
-        val codeSQL = "SELECT code FROM auth_codes WHERE created = (SELECT max(created) FROM auth_codes);";
-        val runner = new QueryRunner();
-        String verificationCode = "";
+        return run.query(connection(),
+                "SELECT code FROM auth_codes WHERE created = (SELECT max(created) FROM auth_codes);",
+                new ScalarHandler<>());
+    }
 
-        try (
-                val conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/app", "app", "pass"
-                );
-        ) {
-            val code = runner.query(conn, codeSQL, new ScalarHandler<>());
-            System.out.println(code);
-            verificationCode = (String) code;
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    @SneakyThrows
+    private static Connection connection() {
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass");
+    }
 
-        return verificationCode;
+    @SneakyThrows
+    public static void wipeEverything() {
+        run.execute(connection(), "TRUNCATE auth_codes");
+        run.execute(connection(), "TRUNCATE cards;");
+        run.execute(connection(), "TRUNCATE card_transactions;");
+        run.execute(connection(), "DELETE FROM users WHERE status LIKE '%ive';");
     }
 }
